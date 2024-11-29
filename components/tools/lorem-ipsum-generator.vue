23bbd0f852e040abb1bfa8347e1521bc9ec574ee
柳问星 <lwx_redstone@163.com>
2024-11-29 13:51:32 +0800

    feat: 添加中文版本的乱数假文生成器

--- components/tools/lorem-ipsum-generator.vue
+++ components/tools/lorem-ipsum-generator.vue
@@ -77,6 +77,7 @@ defineOptions({
         description: "Lorem ipsum 是一种占位符文本，通常用于演示文档或字体的视觉形式，而不依赖于有意义的内容",
         category: ToolCategory.text,
         related: [
+            ToolName['chinese-lorem-ipsum'],
             {
                 title: "IT-TOOLS - Lorem ipsum生成器",
                 url: "https://www.aiotools.top/lorem-ipsum-generator"
