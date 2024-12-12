107077c0a424a17a8595c53192fb02d8c45d811d
柳问星 <lwx_redstone@163.com>
2024-12-12 19:15:32 +0800

    feat: 添加工具文本与 Unicode 互转

---
+++ components/tools/text-to-unicode/content.vue
@@ -0,0 +1,89 @@
+<template>
+    <define-tool-wrapper>
+        <template #input>
+            <n-form :model="model" :rules="rules">
+                <define-tool-area label="基础">
+                    <n-form-item path="transferType" first label="转换类型">
+                        <n-select :options="transferTypeOptions" v-model:value="model.transferType"></n-select>
+                    </n-form-item>
+                    <n-form-item path="ascii" first label="Unicode" v-if="model.transferType === TransferType.text">
+                        <n-input type="textarea" v-model:value="model.unicode" :rows="3"></n-input>
+                    </n-form-item>
+                    <n-form-item path="text" first label="文本" v-if="model.transferType === TransferType.unicode">
+                        <n-input type="textarea" v-model:value="model.text" :rows="3"></n-input>
+                    </n-form-item>
+                </define-tool-area>
+            </n-form>
+        </template>
+        <template #output>
+            <div class=" break-all">
+                {{ res }}
+            </div>
+        </template>
+        <template #actions>
+            <n-space>
+                <n-button size="small" @click="copy(res)">复制</n-button>
+            </n-space>
+        </template>
+    </define-tool-wrapper>
+</template>
+
+<script setup lang="ts">
+import type { FormRules } from "naive-ui";
+
+enum TransferType {
+    text = "text",
+    unicode = "unicode"
+}
+
+export type Model = {
+    transferType: TransferType,
+    text: string,
+    unicode: string
+}
+
+const model = reactive<Model>({
+    transferType: TransferType.text,
+    text: "",
+    unicode: "",
+})
+
+onMounted(() => {
+    const text = "你好";
+    model.text = text;
+    model.unicode = convertTextToUnicode(text)
+})
+
+const copy = useCopy()
+
+const transferTypeOptions = defineSelectOptionList<Record<TransferType, unknown>>({
+    [TransferType.text]: { label: "转文本" },
+    [TransferType.unicode]: { label: "转Unicode" },
+})
+
+const rules: FormRules = {
+}
+
+const convertTextToUnicode = (text: string): string => {
+    return text.split('').map(value => `&#${value.charCodeAt(0)};`).join('');
+}
+
+const convertUnicodeToText = (unicodeStr: string): string => {
+    return unicodeStr.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+}
+
+const res = computed(() => {
+    const transferType = model.transferType;
+    const text = model.text;
+    const ascii = model.unicode;
+    let textRes = "";
+    if (transferType === TransferType.text) {
+        textRes = convertUnicodeToText(ascii)
+    }
+    if (transferType === TransferType.unicode) {
+        textRes = convertTextToUnicode(text)
+    }
+    return textRes
+})
+
+</script>
---
+++ components/tools/text-to-unicode/meta.ts
@@ -0,0 +1,5 @@
+export default defineToolMeta({
+  title: "文本与 Unicode 互转",
+  description: `文本与 Unicode 互转`,
+  category: ToolCategory.dev,
+});
