d3d512bc09bf3736cb603e9af4e8fce05919fbce
柳问星 <lwx_redstone@163.com>
2024-12-12 21:51:37 +0800

    feat: 添加 JSON 和 TOML 互转

---
+++ components/tools/json-to-toml/meta.ts
@@ -0,0 +1,6 @@
+export default defineToolMeta({
+  title: "JSON 转 TOML",
+  description: "JSON 转 TOML",
+  category: ToolCategory.dev,
+  related: [ToolName["toml-to-json"]],
+});
---
+++ components/tools/toml-to-json/meta.ts
@@ -0,0 +1,6 @@
+export default defineToolMeta({
+  title: "TOML 转 JSON",
+  description: "TOML 转 JSON",
+  category: ToolCategory.dev,
+  related: [ToolName["json-to-toml"]],
+});
