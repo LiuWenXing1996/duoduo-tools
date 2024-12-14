908f2aea17f777dffd2951ce1a32c5fb6a0de244
柳问星 <lwx_redstone@163.com>
2024-12-15 00:31:58 +0800

    docs: 更新文案

--- components/tools/url-parser/meta.ts
+++ components/tools/url-parser/meta.ts
@@ -12,7 +12,7 @@ const getAllMethods = () => {
 
 export default defineToolMeta({
   title: "URL字符串解析",
-  description: `解析输入的 url 字符串，以 ${exampleUrl} 为例, 支持以下属性：
+  description: `解析输入的 url 字符串，以 <${exampleUrl}> 为例, 支持以下属性：
 ${getAllMethods()};
 `,
   category: ToolCategory.dev,
